bb1e606c3d8b7bde60cd1d2747b89484727f4c00
Tushar Solanki <141443573+Tusharsolanki33@users.noreply.github.com>
2025-07-31 17:53:41 +0530

    Update Footer.jsx
    Update footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => {
 
         {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2025 Tarun Kaushik. All rights reserved.
+          © 2025 Tushar Solanki. All rights reserved.
         </p>
       </div>
     </footer>
